71e2bf5de56f4612ce6931b17c1bfc34e8c8a204
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-02-25 23:00:08 +0530

    ..
    .

---
+++ Python/Hello World.py
@@ -0,0 +1,14 @@
+# Here is a sample line of code that can be executed in Python:
+
+#print("Hello, World!")
+#You can just as easily store a string as a variable and then print it to stdout:
+# Input Format
+
+# You do not need to read any input in this challenge.
+
+# Output Format
+
+# Print Hello, World! to stdout.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT  
+  print("Hello, World!")
