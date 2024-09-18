44cbb023c3694203ed8b6059b55a0898968f5513
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2024-09-18 14:12:37 +0530

    Create Sales by Match.py

---
+++ Algorithms/Implementation/Sales by Match.py
@@ -0,0 +1,53 @@
+```python
+There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+Example
+
+
+There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+Function Description
+
+Complete the sockMerchant function in the editor below.
+
+sockMerchant has the following parameter(s):
+
+int n: the number of socks in the pile
+int ar[n]: the colors of each sock
+Returns
+
+int: the number of pairs
+Input Format
+
+The first line contains an integer , the number of socks represented in .
+The second line contains  space-separated integers, , the colors of the socks in the pile.
+
+Constraints
+
+ where 
+Sample Input
+
+STDIN                       Function
+-----                       --------
+9                           n = 9
+10 20 20 10 10 30 50 10 20  ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+Sample Output
+
+3
+```
+
+### Solution
+```python
+def sockMerchant(n, ar):
+    f = {}
+    count = 0
+    for i in ar:
+        if i in f:
+            f[i] += 1
+        else :
+            f[i] = 1
+        if f[i] == 2:
+            count += 1
+            f[i] = 0
+    return count   
+```
