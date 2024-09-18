28f66331ffecc8c18e1374165f805056ae6f1083
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2024-09-18 16:36:58 +0530

    Update Minimum Distances.py

--- Algorithms/Implementation/Minimum Distances.py
+++ Algorithms/Implementation/Minimum Distances.py
@@ -1,3 +1,4 @@
+## Question:
 ```python
 The distance between two array values is the number of indices between them. Given , find the minimum distance between any pair of equal elements in the array. If no such value exists, return .
 
